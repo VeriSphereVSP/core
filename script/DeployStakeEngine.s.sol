0f53f933b4c79657ab7cc406d1236c3d862dd6f8
VSP-0 <v0anon@proton.me>
2025-12-18 20:03:20 +0200

    Grant mint/burn permissions to StakeEngine
    Added permission granting for StakeEngine to mint and burn tokens.

--- script/DeployStakeEngine.s.sol
+++ script/DeployStakeEngine.s.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
+
 import "../src/StakeEngine.sol";
+import "../src/VSPToken.sol";
+import "../src/authority/Authority.sol";
 
 contract DeployStakeEngine is Script {
     function run() external {
@@ -13,6 +16,13 @@ contract DeployStakeEngine is Script {
 
         StakeEngine engine = new StakeEngine(vsp);
         console.log("StakeEngine deployed at:", address(engine));
+
+        // Grant StakeEngine permissions to mint/burn (must be called by Authority owner)
+        Authority auth = VSPToken(vsp).authority();
+        auth.setMinter(address(engine), true);
+        auth.setBurner(address(engine), true);
+
+        console.log("Granted minter+burner to StakeEngine on Authority:", address(auth));
 
         vm.stopBroadcast();
     }
